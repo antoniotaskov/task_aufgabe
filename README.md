f2fafbf80531f1029fbc159d3f91b483332c2172
antoniotaskov <antoniotaskov87@gmail.com>
2024-07-09 17:07:09 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# task_aufgabe
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/task_aufgabe/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.task_aufgabe
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
